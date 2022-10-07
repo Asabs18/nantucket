3fde2dacc817c7fb61a2249e077ef119662b3311
Asabs18 <aidan.j.sabatini@icloud.com>
2022-10-07 09:51:55 -0400

    Create random.h

---
+++ c-version/random.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+// Random number between zero and one
+double rand01() {
+  static int needsrand = 1;
+  if (needsrand) {
+    srand(time(NULL));
+    needsrand = 0;
+  }
+  return (rand() / (1.0 + RAND_MAX));
+}
+// Normal distribution:
+double normal() {
+  int nroll = 12;
+  double sum = 0;
+  int i;
+  for (i = 0; i < nroll; i++) {
+    sum += rand01();
+  }
+  return (sum - 6.0);
+}
